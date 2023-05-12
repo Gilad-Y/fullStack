404b4b6415996f24d2b769059f2d5e4706c6a183
Gilad-Y <yitzhakgilad64@gmail.com>
2023-05-13 00:05:54 +0300

    need to solve the starting problem

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # fullStack
+
 full stack
+to instal mui use
+npm i @mui/material @emotion/react @emotion/styled @mui/icons-material
--- mystuff/tirgul/src/Components/Pages/taskBoard/taskBoard.tsx
+++ mystuff/tirgul/src/Components/Pages/taskBoard/taskBoard.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { store } from "../../redux/store";
-import { downloadTask } from "../../redux/tasksReducer";
+import { addTask, downloadTask } from "../../redux/tasksReducer";
 import "./taskBoard.css";
 import { Tasks } from "../../modal/taskModal";
 import SingleTask from "./singleTask/singleTask";
